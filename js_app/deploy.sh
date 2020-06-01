139f1dbfc9bc44c2f5b41cfdc87f97ff79d09a1a
Kei Sato <ckeisato@gmail.com>
2020-05-31 21:00:50 -0700

    Update deploy

--- js_app/deploy.sh
+++ js_app/deploy.sh
@@ -3,5 +3,5 @@ gulp build
 cd public
 git init
 git add -A
-git commit -m 'update cksdotcom'
-git push -f git@github.com:ckeisato/cksdotcom.git master:gh-pages
+git commit -m 'update movieReviews'
+git push -f git@github.com:ckeisato/movieReviews.git master:gh-pages
