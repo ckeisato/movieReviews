f0dfb9b92ec9e9143681c8dccc5179fa7049eecc
Kei Sato <ckeisato@gmail.com>
2020-06-12 18:23:55 -0700

    update functions to be global variables

--- app_test/cloud.py
+++ app_test/cloud.py
@@ -8,6 +8,16 @@ BUCKET = os.environ['BUCKET']
 VECTORIZER_FILENAME = os.environ['VECTORIZER_FILENAME']
 MODEL_FILENAME = os.environ['MODEL_FILENAME']
 
+client = storage.Client()
+bucket = client.get_bucket(BUCKET)
+
+vectorizer_blob = bucket.get_blob(VECTORIZER_FILENAME)
+model_blob = bucket.get_blob(MODEL_FILENAME)
+
+vectorizer = pickle.loads(vectorizer_blob.download_as_string())
+model = pickle.loads(model_blob.download_as_string())
+
+
 def predict_text(request):
     headers = {
         'Access-Control-Allow-Origin': '*',
@@ -18,15 +28,7 @@ def predict_text(request):
         request_json = request.get_json(silent=True)
         if request_json and 'text' in request_json:
             text = request_json['text']
-            client = storage.Client()
-            bucket = client.get_bucket(BUCKET)
-        
-            vectorizer_blob = bucket.get_blob(VECTORIZER_FILENAME)
-            model_blob = bucket.get_blob(MODEL_FILENAME)
-        
-            vectorizer = pickle.loads(vectorizer_blob.download_as_string())
-            model = pickle.loads(model_blob.download_as_string())
-            
+
             vectorized_text = vectorizer.transform([text])
             result = model.predict_proba(vectorized_text)
             return ((jsonify({ 'negative': result[0][0], 'positive': result[0][1]})), 200, headers)
